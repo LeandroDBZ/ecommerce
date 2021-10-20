84b3470546f79e727213bb23a88b6c7e9fac2f04
Leandro Araujo <leandro.eries@gmail.com>
2021-10-19 22:19:19 -0300

    Configurando tela de login administrativo

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Ecommerce\Page;
-use Ecommerce\PageAdmin;
+use \Ecommerce\PageAdmin;
+use \Ecommerce\Model\User;
 
 $app = new Slim();
 
@@ -13,14 +14,34 @@ $app->get('/', function() {
     
 	$page = new Page();
 	$page->setTpl("index");
-
 });
 
 $app->get('/admin', function() {
-    
+    User::verifyLogin();
 	$page = new PageAdmin();
 	$page->setTpl("index");
+});
 
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+    
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+	User::logout();
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
--- vendor/ecommerce/php-classes/src/Page.php
+++ vendor/ecommerce/php-classes/src/Page.php
@@ -7,6 +7,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+		"footer"=>true,
         "data"=>[]
     ];
 
@@ -20,7 +22,8 @@ class Page {
         Tpl::configure( $config );
         $this->tpl = new Tpl;
         $this->setData($this->options["data"]);
-        $this->tpl->draw("header");
+
+        if ($this->options["header"] === true) $this->tpl->draw("header");
     }
 
     private function setData($data = array()){
@@ -35,6 +38,6 @@ class Page {
     }
 
     public function __destruct(){
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 }
